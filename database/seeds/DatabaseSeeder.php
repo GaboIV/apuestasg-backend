9f246515a5dddafb7f61da2db2adf42b87852eb7
Gabriel Caraballo Moya <backend1peruapps@gmail.com>
2019-09-30 16:12:47 -0500

    Agregado modelo de países, estados, ciudades y parroquias de Venezuela / Seeds para tablas de ubicación / Modelo de equipos.

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,15 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        // Países
+        DB::unprepared(file_get_contents(public_path() . '/seeds/countries.sql'));
+        // Estados de Venezuela
+        DB::unprepared(file_get_contents(public_path() . '/seeds/states.sql'));
+        // Ciudades de Venezuela
+        DB::unprepared(file_get_contents(public_path() . '/seeds/cities.sql'));
+        // Parroquias de Venezuela
+        DB::unprepared(file_get_contents(public_path() . '/seeds/parishes.sql'));
+
         // La creación de datos de roles debe ejecutarse primero
         $this->call(RoleSeeder::class);
         // Los usuarios necesitarán los roles previamente generados
