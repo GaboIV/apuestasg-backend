ef08834cd3770e5021b730b973895091d30c0092
Gabriel Caraballo <gcaraballoperuapps@gmail.com>
2020-05-23 01:58:23 -0500

    Corregido: sincronización de carreras

--- app/Http/Controllers/Admin/Horses/RacecourseController.php
+++ app/Http/Controllers/Admin/Horses/RacecourseController.php
@@ -46,32 +46,52 @@ class RacecourseController extends ApiController{
 
         $racecourse = Racecourse::find($id);
 
-        if (isset($racecourse->url)) {
+        if (isset($racecourse)) {
             $client = new \GuzzleHttp\Client(['verify' => false, 'headers' => [
                 'Content-Type' => 'text/plain'
             ]]);
 
-            $url = 'https://www.drf.com/entries/entryDetails/id/' . $racecourse->acro . '/country/' . $racecourse->country->acro_3 . '/date/' . $date;
+            $date_2 = explode("-", $date);
 
-            // dd($url);
+            $url_1 = 'https://www.drf.com/entries/entryDetails/id/' . $racecourse->acro . '/country/' . $racecourse->country->acro_3 . '/date/' . $date;
 
-            $tracks = json_decode($client->request('GET', $url)->getBody());    
+            $url_2 = 'https://xpbapi.drf.com/races/'. $date_2['2'] . "-" . $date_2['0'] . "-" . $date_2['1'] . '/track/' . $racecourse->acro;
+
+            // dd($url_1);
+
+            $tracks_1 = json_decode($client->request('GET', $url_1)->getBody());    
+
+            $tracks_2 = json_decode($client->request('GET', $url_2)->getBody()); 
                 
-            // dd($tracks);
+            // dd($tracks_1);
 
-            foreach ($tracks->races as $trk) {
+            foreach ($tracks_1->races as $key_trk => $trk) {
 
                 // $country = Country::where('acro_2', $trk->Country)->OrWhere('acro_3', $trk->Country)->first(); 
 
-            //    dd($trk);
+                // dd($trk);
+
+                if ($trk->distanceUnit == ''){
+                    $data_trk = explode("|", $trk->raceTypeDescription);
+
+                    if (strpos($data_trk[0], "f") != false){
+                        $trk->distanceUnit = "F";
+                        $distance_explode = explode("f", trim($data_trk[0]));
+                        $trk->distanceValue = $this->parseFraction($distance_explode[0]);
+                    } elseif (strpos($data_trk[0], "m") != false){
+                        $trk->distanceUnit = "M";
+                        $distance_explode = explode("m", trim($data_trk[0]));
+                        $trk->distanceValue = $this->parseFraction($distance_explode[0]);
+                    }
+                }
                 
                 if ($trk->distanceUnit == "F") {
                     $distance = ($trk->distanceValue) * 201;
                 } elseif ($trk->distanceUnit == "Y") {
                     $distance = ($trk->distanceValue / 1.094);
                 } elseif ($trk->distanceUnit == "M") {
-                    $distance = $trk->distanceValue;
-                }
+                    $distance = $trk->distanceValue * 1609.34;
+                }                
 
                 if ($trk->surfaceDescription == "Turf") {
                     $surface = "Grama";
@@ -81,20 +101,19 @@ class RacecourseController extends ApiController{
 
                 $raceKey = $trk->raceKey;
 
-                $s = $raceKey->raceDate->year . "-" . ($raceKey->raceDate->month + 1) . "-" . $raceKey->raceDate->day . " " . $trk->postTime;
-                $stro_date = strtotime($s);
-                $posttime = date('Y-m-d H:i:s', $stro_date);
+                // $s = $raceKey->raceDate->year . "-" . ($raceKey->raceDate->month + 1) . "-" . $raceKey->raceDate->day . " " . $trk->postTime;
+                // $stro_date = strtotime($s);
+                $posttime = date('Y-m-d H:i:s', ($trk->postTimeLong / 1000)); 
 
                 $career = Career::updateOrCreate([
                     "racecourse_id" => $racecourse->id,
-                    "date" => date('Y-m-d', $stro_date),
+                    "date" => date('Y-m-d', ($trk->postTimeLong / 1000)),
                     "number" => $raceKey->raceNumber
-                ],
-                [
+                ],[
                     "name" => trim($trk->raceClass) . ", " . trim($trk->sexRestrictionDescription) . ", " . trim($trk->ageRestrictionDescription),
                     "title" => $trk->raceTypeDescription,
                     "posttime" => $posttime,
-                    "distance" => $distance,
+                    "distance" => number_format($distance, 0, '.', ''),
                     "surface" => $surface ?? null,
                     "status" => 1,
                     "grade" => null,
@@ -104,12 +123,14 @@ class RacecourseController extends ApiController{
                     "record" => null
                 ]);
 
-                foreach ($trk->runners as $ins) { 
+                // dd($tracks_2->tracks[$key_trk]->runners);
+
+                foreach ($tracks_2->tracks[$key_trk]->runners as $ins) { 
                     
-                    if ($ins->jockey) {
+                    if (isset($ins->jockey)) {
                         $jockey = $ins->jockey;
 
-                        $jockeyName = trim(($jockey->firstName ? $jockey->firstName . " " : "") . ($jockey->middleName ? $jockey->middleName . " " : "") . ($jockey->lastName ? $jockey->lastName . " " : ""));
+                        $jockeyName = trim(($jockey->firstName ? $jockey->firstName . " " : "") . ($jockey->lastName ? $jockey->lastName . " " : ""));
 
                         $jockey = Jockey::firstOrCreate(
                             [ "name" =>  $jockeyName],
@@ -120,56 +141,68 @@ class RacecourseController extends ApiController{
                     if ($ins->trainer) {     
                         $trainer = $ins->trainer;
 
-                        $trainerName = trim(($trainer->firstName ? $trainer->firstName . " " : "") . ($trainer->middleName ? $trainer->middleName . " " : "") . ($trainer->lastName ? $trainer->lastName . " " : ""));
+                        $trainerName = trim(($trainer->firstName ? $trainer->firstName . " " : "") . ($trainer->lastName ? $trainer->lastName . " " : ""));
 
                         $trainer = Trainer::firstOrCreate(  
                             [ "name" =>  $trainerName],
                             [ "name_id" => $trainerName, "country_id" => $racecourse->country->id ]                    
                         );
+                    } else {
+                        $jockey = null;
                     }
 
-                    if ($trk->sexRestrictionDescription == 'Horses' || $trk->sexRestrictionDescription == 'Geldings' || $trk->sexRestrictionDescription == 'Colts') {
-                        $sexHorse = "M";
-                    } elseif ($trk->sexRestrictionDescription == 'Mares' || $trk->sexRestrictionDescription == 'Fillies' || $trk->sexRestrictionDescription == 'Females') {
-                        $sexHorse = "F";
-                    } else {
-                        $sexHorse = "N";
-                    }
+                    // if ($trk->sexRestrictionDescription == 'Horses' || $trk->sexRestrictionDescription == 'Geldings' || $trk->sexRestrictionDescription == 'Colts') {
+                    //     $sexHorse = "M";
+                    // } elseif ($trk->sexRestrictionDescription == 'Mares' || $trk->sexRestrictionDescription == 'Fillies' || $trk->sexRestrictionDescription == 'Females') {
+                    //     $sexHorse = "F";
+                    // } else {
+                    //     $sexHorse = "N";
+                    // }
                     
                     $horse = Horse::updateOrCreate(
                         [
-                            "name" => $ins->horseName
+                            "name" => $ins->name
                         ],
                         [
-                            "sex" => $sexHorse ?? null,
-                            "color" => $ins->Color ?? null,
-                            "birthday" => null
+                            "sex" => $ins->gender ?? null,
+                            "color" => $ins->color ?? null,
+                            "birthday" => $ins->yearOfBirth ? $ins->yearOfBirth . "-01-01" : null
                         ]
                     );
         
                     Inscription::updateOrCreate(
                         [
                             "career_id" => $career->id,
-                            "number" => $ins->programNumberStripped
+                            "number" => $ins->programNumber
                         ],
                         [
                             "horse_id" => $horse->id,
-                            "jockey_id" => $jockey->id,
+                            "jockey_id" => $jockey->id ?? null,
                             'trainer_id' => $trainer->id,
-                            'position' => $ins->postPos,
-                            'odd' => str_replace(".", "/", $ins->morningLineOdds),
-                            'weight' => (round(($ins->weight / 2.205) * 2) / 2),
-                            'medicines' => $ins->medication,
-                            'implements' => $ins->equipment
+                            'position' => $ins->postPosition,
+                            'odd' => $ins->MLOdd,
+                            'weight' => (round(($ins->wtCarried / 2.205) * 2) / 2),
+                            'medicines' => $ins->medication ?? null,
+                            'implements' => $ins->equipment ?? null
                         ]
                     );
                 }  
             }            
 
-            return $this->successResponse("Se sincronizaron " . count($tracks->races). " carreras.", 200);
+            return $this->successResponse("Se sincronizaron " . count($tracks_1->races). " carreras.", 200);
         } else {
             $this->errorResponse("Hipodromo sin link de actualización", 406);
         }
+    }
+
+    public function parseFraction(string $fraction): float 
+    {
+        if(preg_match('#(\d+)\s+(\d+)/(\d+)#', $fraction, $m)) {
+            return ($m[1] + $m[2] / $m[3]);
+        } else if( preg_match('#(\d+)/(\d+)#', $fraction, $m) ) {
+            return ($m[1] / $m[2]);
+        }
+        return (float) $fraction;
     }
 
     // public function syncCareers ($id) {
