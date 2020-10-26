bc6c565f36ca039f21ab8f636d6a8e5233dea07b
Gabriel Caraballo <gcaraballoperuapps@gmail.com>
2020-10-25 23:14:13 -0500

    Corregido: dependencia de .env para websockets

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -38,9 +38,9 @@ return [
             'options' => [
                 'cluster' => env('PUSHER_APP_CLUSTER'),
                 'host' => '127.0.0.1',
-                'encrypted' => true,
+                'encrypted' => env('PUSHER_APP_ENCRYPTED'),
                 'port' => 6001,
-                'scheme' => 'https',
+                'scheme' => env('PUSHER_APP_SCHEME'),
                 'curl_options' => [
                     CURLOPT_SSL_VERIFYHOST => 0,
                     CURLOPT_SSL_VERIFYPEER => 0,
